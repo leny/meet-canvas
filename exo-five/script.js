2950b52a2ec61d2b8e97c9df1bd35cb7894dcc17
leny <info@flatland.be>
2017-03-27 11:19:54 +0200

    Implement exo-five solution

--- exo-five/script.js
+++ exo-five/script.js
@@ -1,1 +1,28 @@
 // hepl-mmi/meet-canvas - exo-five
+
+let oApp;
+
+class ExoFiveCanvApp extends CanvApp {
+    drawStar() {
+        let { context, width, height } = this,
+            iStarSize = 200,
+            iSteps = 13,
+            iCurrentStep = 0;
+
+        context.translate( width / 2, height / 2 );
+
+        context.strokeStyle = "red";
+        while( ++iCurrentStep <= iSteps ) {
+            context.rotate( Math.PI * 2 / iSteps );
+            context.strokeRect( iStarSize / 2 * -1, iStarSize / 2 * -1, iStarSize, iStarSize );
+        }
+
+    }
+
+    draw() {
+        this.drawStar();
+    }
+}
+
+oApp = new ExoFiveCanvApp( "app" );
+oApp.draw();
